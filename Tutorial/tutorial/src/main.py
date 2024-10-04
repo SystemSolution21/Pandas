97d3317d8b05f19f9a2ce6eee9eb1d45e0c4f497
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2024-10-04 21:01:27 +0900

    Added Pandas Series

--- Tutorial/tutorial/src/main.py
+++ Tutorial/tutorial/src/main.py
@@ -3,6 +3,20 @@ import sys
 import pandas as pd
 
 print("-------------------------------------------------------------------------------------")
+# Pandas Series
+print("Pandas Series")
+ser_lst: list = [1, 3, 2]
+df = pd.Series(ser_lst)
+print(df)
+
+# Create Labels for Series
+print("Create Labels for Series")
+ser_lbl: list = ["x", "y", "z"]
+ser_lst_lbl = pd.Series(ser_lst, index = ser_lbl)
+print(ser_lst_lbl)
+print("-------------------------------------------------------------------------------------")
+
+print("-------------------------------------------------------------------------------------")
 # Pandas DataFrame
 print("Pandas DataFrame")
 df = pd.DataFrame({"A": [1, 2, 3, 4, 5]})
@@ -20,3 +34,4 @@ csv_file = os.path.join(current_dir, "..", "data", "data.csv")
 print("Pandas read csv file")
 data = pd.read_csv(csv_file)
 print(data.head())
+print("-------------------------------------------------------------------------------------")
